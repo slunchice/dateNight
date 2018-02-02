a1a4f5e860013b7ac873f839d1f6afafcb8b373d
FuzzyJones <donald.jones83@gmail.com>
2018-02-02 17:44:07 -0500

    commented in 2 api keys in the JS, made reference links in the html

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+// yelp api_____
+// I requested an api key and it is pasted in the line below
+// bcs2fOhux1UxgJZYOjtqrLNWDZ6vNIgGrG_ESgiA64aFLubG3z7uL5vFvTMqn5onZA8hqGZse6UiSqlB0oLm5UIjzjXUfqCsqLI63vCJhjJSQW84sTBBy-v9A8lzWnYx
+// not sure if we need client ID but its pasted below
+// rB_Ksk_aj6pA6fD1QKzXIw
+// I believe the below url is what we link our api to when we search with our GET
+// https://api.yelp.com/v3/businesses/search
+
+// eventful api_____
+// fandango didnt have a public api anymore, I think eventful could be a better option since it does movies and other events like concerts too
+// I requested an api key and it is pasted in the line below
+// xzCLvXpkc5JmtDcX
+// the url below is the tutorial how to use the eventful api
+// https://api.eventful.com/tools/tutorials/search
+// here is the eventful url if you wanna check out the site to see if it would work for us http://charlotte.eventful.com/events
